2bf4c95445e1742f7bf387e288d9f2ab7944f619
Sierra Chapman <siechap@gmail.com>
2020-07-21 21:21:07 -0700

    write SQL to set up database

--- schema.sql
+++ schema.sql
@@ -1,9 +1,29 @@
 -- To create the database and tables, paste the contents of this file into MySQL Workbench and run. --
 
 -- Create "employee_management_db" database --
+CREATE database employee_management_db;
+
+USE employee_management_db;
 
 -- Create "employee" table --
+CREATE TABLE employee (
+  id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+  first_name VARCHAR(30) NOT NULL,
+  last_name VARCHAR(30) NOT NULL,
+  role_id INT NOT NULL,
+  manager_id INT NULL
+);
 
 -- Create "role" table --
+CREATE TABLE role (
+  id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+  title VARCHAR(30) NOT NULL,
+  salary DECIMAL NOT NULL,
+  department_id INT NOT NULL
+);
 
 -- Create "department" table --
+CREATE TABLE department (
+  id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+  name VARCHAR(30) NOT NULL
+);
